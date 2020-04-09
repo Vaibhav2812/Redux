110bdf2b075917f3f952055dd0a95d579ae32f83
vaibhav_patil <vaibhav.laxmanpatil@narayanahealth.org>
2020-04-10 00:09:39 +0530

    Working with dispatch, subscribe and getState

---
+++ index.js
@@ -0,0 +1,39 @@
+import { createStore } from 'https://unpkg.com/redux@4.0.5/es/redux.mjs';
+
+const initialState = {
+	buttonClicked: 'no',
+	divVisible: 'no'
+}
+
+function rootReducer(state=initialState, action) {
+	switch (action.type) {
+		case "BUTTON_CLICKED":
+			return {...state, buttonClicked: 'yes'}	
+		case "DIV_VISIBLE":
+			return {...state, divVisible: 'yes'}		
+		default:
+			return state;
+	}
+}
+
+const store = createStore(rootReducer);
+
+const button = document.getElementById('my-btn');
+
+button.addEventListener('click', ()=> {
+	const btnClickedAction = {
+		type: 'BUTTON_CLICKED'
+	}
+	const divVisibleAction = {
+		type: 'DIV_VISIBLE'
+	}
+	store.dispatch(btnClickedAction);
+	store.dispatch(divVisibleAction);
+})
+
+store.subscribe(() => {
+  if(store.getState().divVisible === 'yes') {
+   const div = document.getElementById('my-div');
+   div.style.display = 'block';
+  }
+})
